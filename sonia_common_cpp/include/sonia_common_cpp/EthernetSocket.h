109fd2c5cea214d8b1faf1bd81ba95adefa90633
Nimai Jariwala <nimai.jariwala.1@ens.etsmtl.ca>
2024-05-04 18:26:23 -0400

    FEAT: Working Ethernet Driver

--- sonia_common_cpp/include/sonia_common_cpp/EthernetSocket.h
+++ sonia_common_cpp/include/sonia_common_cpp/EthernetSocket.h
@@ -1,4 +1,4 @@
-#pragma once   
+#pragma once
 
 #include <string>
 #include <cstring>
@@ -6,29 +6,75 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-
 namespace sonia_common_cpp
 {
     class EthernetSocket
     {
-        public:
-            EthernetSocket();
-            ~EthernetSocket();
+    public:
+        /**
+         * @brief Construct a new Ethernet Socket object
+         *
+         * @param dataSize Size of message tram.
+         */
+        EthernetSocket(size_t dataSize);
+        ~EthernetSocket();
 
-            bool ConnectTCP(std::string address, int port);
-            bool ConnectUDP(int port);
-            bool Receive();
-            bool Send(std::vector<uint8_t> data);
+        /**
+         * @brief Connect to a UDP Port.
+         *
+         * @param port Port to connect to.
+         * @return true If successful.
+         * @return false If failed to connect.
+         */
+        bool ConnectUDP(int const port);
 
-            std::vector<uint8_t> GetRawData();
-            
-        private:
+        /**
+         * @brief Connect to a TCP Port.
+         *
+         * @param addr IP address to connect to.
+         * @param port Port to connect to.
+         * @return true if successful.
+         * @return false if failed to connect.
+         */
+        bool ConnectTCP(std::string const addr, int const port);
 
-            int _socketUDP;
-            int _socketTCP;
-            sockaddr_in _server, _dvl;
-            std::vector<uint8_t> _data;
+        /**
+         * @brief Send Data.
+         *
+         * @param data Data to send.
+         * @return true if successful.
+         * @return false if failed to send.
+         */
+        bool Send(const char *data);
 
-    };//End classe
+        /**
+         * @brief Wait for data from UDP Port.
+         *
+         * @return true If receive successful.
+         * @return false If bad receive.
+         */
+        bool ReceiveUDP();
 
-}//End namespace
+        /**
+         * @brief Wait for data from TCP Port.
+         *
+         * @return true If receive successful.
+         * @return false If bad receive.
+         */
+        bool ReceiveTCP();
+
+        /**
+         * @brief Get the data from receive.
+         *
+         * @return char* parsed data.
+         */
+        char *GetRawData();
+
+    private:
+        bool _receive(int socket);
+        int _socketUDP, _socketTCP;
+        sockaddr_in _server, _dvl;
+        char *_data;
+        size_t _size;
+    };
+} // End namespace
--- sonia_common_cpp/src/EthernetSocket.cpp
+++ sonia_common_cpp/src/EthernetSocket.cpp
@@ -1,67 +1,115 @@
 #include "EthernetSocket.h"
+#include <unistd.h>
 
 namespace sonia_common_cpp
 {
-    EthernetSocket::EthernetSocket()
+    EthernetSocket::EthernetSocket(size_t dataSize)
+        : _data{new char[dataSize]}, _size{dataSize}
     {
-
+        if (!_data)
+        {
+            this->~EthernetSocket();
+        }
     }
+
+    //------------------------------------------------------------------------------
+    //
     EthernetSocket::~EthernetSocket()
     {
-        
+        close(_socketTCP);
+        close(_socketUDP);
+        delete (_data);
     }
 
-    bool EthernetSocket::ConnectTCP(std::string address, int port)
+    //==============================================================================
+    // M E T H O D   S E C T I O N
+
+    //------------------------------------------------------------------------------
+    //
+    bool EthernetSocket::ConnectUDP(int port)
     {
-        memset(&_server,0,sizeof(_server));
-        _server.sin_addr.s_addr= inet_addr(address.c_str());
+
+        bzero(&_server, sizeof(_server));
+        _server.sin_addr.s_addr = htonl(INADDR_ANY);
+        _server.sin_family = AF_INET;
+        _server.sin_port = htons(port);
+
+        _socketUDP = socket(AF_INET, SOCK_DGRAM, 0);
+        if (_socketUDP < 0)
+        {
+            return false;
+        }
+        if (bind(_socketUDP, (struct sockaddr *)&_server, sizeof(_server)) < 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    //------------------------------------------------------------------------------
+    //
+    bool EthernetSocket::ConnectTCP(std::string addr, int port)
+    {
+
+        bzero(&_server, sizeof(_server));
+
+        _server.sin_addr.s_addr = inet_addr(addr.c_str());
         _server.sin_family = AF_INET;
         _server.sin_port = htons(port);
 
         _socketTCP = socket(AF_INET, SOCK_STREAM, 0);
-        if(connect(_socketTCP, (struct sockaddr *) &_server, sizeof(_server))<0)
+        if (_socketTCP < 0)
+        {
+            return false;
+        }
+        if (connect(_socketTCP, (struct sockaddr *)&_server, sizeof(_server)) < 0)
         {
             return false;
         }
         return true;
     }
-    bool EthernetSocket::ConnectUDP(int port)
-    {
-        memset(&_server,0,sizeof(_server));
-        _server.sin_addr.s_addr= htonl(INADDR_ANY);
-        _server.sin_family = AF_INET;
-        _server.sin_port = htons(port);
 
-        _socketTCP = socket(AF_INET, SOCK_DGRAM, 0);
-        if(connect(_socketTCP, (struct sockaddr *) &_server, sizeof(_server))<0)
-        {
-            return false;
-        }
-        return true;
-
-    }
-
-    bool EthernetSocket::Receive()
+    //------------------------------------------------------------------------------
+    //
+    bool EthernetSocket::_receive(int socket)
     {
         socklen_t len = sizeof(_dvl);
-        if(recvfrom(_socketUDP, &_data, _data.size(),0,(struct sockaddr*)&_dvl, &len)<0)
+        if (recvfrom(socket, _data, _size, 0, (struct sockaddr *)&_dvl, &len) < 0)
         {
             return false;
         }
         return true;
     }
-    bool EthernetSocket::Send(std::vector<uint8_t> data)
+
+    //------------------------------------------------------------------------------
+    //
+    bool EthernetSocket::ReceiveUDP()
     {
-        if(send(_socketTCP, &data, data.size(),0)<0){
-            return true;
-        }
-        return false;
+        return _receive(_socketUDP);
+    }
+    //------------------------------------------------------------------------------
+    //
+    bool EthernetSocket::ReceiveTCP()
+    {
+        return _receive(_socketTCP);
     }
 
-    std::vector<uint8_t> EthernetSocket::GetRawData()
+    //------------------------------------------------------------------------------
+    //
+    bool EthernetSocket::Send(const char *data)
+    {
+        if (send(_socketTCP, data, strlen(data), 0) < 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    //------------------------------------------------------------------------------
+    //
+    char *EthernetSocket::GetRawData()
     {
         return _data;
     }
 
 }
-
