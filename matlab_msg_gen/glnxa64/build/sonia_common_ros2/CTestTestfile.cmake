39b9e6f06e83eae6d7d071b5f76a52930f66d66c
Nimai Jariwala <nimai.jariwala.1@ens.etsmtl.ca>
2024-05-27 19:20:41 -0400

    Fixed merge stuff

--- matlab_msg_gen/glnxa64/build/sonia_common_ros2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nimai/ros2_sonia_ws/src/sonia_common_ros2/matlab_msg_gen/glnxa64/src/sonia_common_ros2
-# Build directory: /home/nimai/ros2_sonia_ws/src/sonia_common_ros2/matlab_msg_gen/glnxa64/build/sonia_common_ros2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
