ac828afb005d14fc0a1be5052ca6a2f99fee6d11
Raees <sonia2@sonia-dory.com>
2025-07-31 14:17:01 -0400

    Added launch file for LITE

---
+++ sonia_common_ros2/launch/LITE1.launch.py
@@ -0,0 +1,53 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+os.environ["AUV"] = "LITE1"
+def generate_launch_description():
+    imu_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('imu_port_manager'), 'launch'),
+            '/launch.py'])
+        )
+   
+    depth_launch = IncludeLaunchDescription(
+       PythonLaunchDescriptionSource([os.path.join(
+           get_package_share_directory('depth_port_manager'), 'launch'),
+           '/launch.py'])
+       )
+    
+    rs485_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('rs485_port_manager'), 'launch'),
+            '/launch.py'])
+        )
+
+    proc_control_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('proc_control'), 'launch'),
+            '/launch.py'])
+        )
+    
+    proc_planner_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('proc_planner_ros2'), 'launch'),
+            '/launch.py'])
+        )
+    zed_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('zed_wrapper'), 'launch'),
+            '/launch.py'])
+        )
+    
+    return LaunchDescription([
+        imu_launch,
+        depth_launch,
+        rs485_launch,
+        proc_control_launch,
+        zed_launch,
+        proc_planner_launch
+    ])
