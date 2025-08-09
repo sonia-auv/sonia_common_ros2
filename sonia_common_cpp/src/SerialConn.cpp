7ff8052fe2bca533ea83428da21706e59bb73ac6
Nimai Jariwala <nimai.jariwala.1@ens.etsmtl.ca>
2025-08-09 00:15:54 -0400

    adding debug print

--- sonia_common_cpp/src/SerialConn.cpp
+++ sonia_common_cpp/src/SerialConn.cpp
@@ -29,16 +29,12 @@ namespace sonia_common_cpp
 
     ssize_t SerialConn::Read(ITramData& tram)
     {
-        std::cout << "READ1" << std::endl;
         SerialTram st_tram = dynamic_cast<SerialTram&>(tram);
-        std::cout << "READ2" << std::endl;
         // tram = (SerialTram)tram;
-        st_tram.data.assign(st_tram.size, 0);
-        std::cout << "READ3" << std::endl;
+        st_tram.data.resize(st_tram.size);  
         _lock.lock();
         ssize_t ret = read(_fd, (st_tram.data.data() + st_tram.offset), st_tram.size);
         _lock.unlock();
-        std::cout << "READ4" << std::endl;
         return ret;
     }
 
