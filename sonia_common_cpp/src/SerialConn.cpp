693578b0da31010c3f672059e2e13f2e5da7b55f
Nimai Jariwala <nimai.jariwala.1@ens.etsmtl.ca>
2025-08-08 23:00:20 -0400

    adding debug print
    adding debug print
    
    adding debug print
    
    adding debug print
    
    adding debug print

--- sonia_common_cpp/src/SerialConn.cpp
+++ sonia_common_cpp/src/SerialConn.cpp
@@ -2,6 +2,7 @@
 
 #include <fcntl.h>
 #include <unistd.h>
+#include <iostream>
 
 namespace sonia_common_cpp
 {
@@ -28,11 +29,11 @@ namespace sonia_common_cpp
 
     ssize_t SerialConn::Read(ITramData& tram)
     {
-        SerialTram *st_tram = dynamic_cast<SerialTram*>(&tram);
+        SerialTram& st_tram = dynamic_cast<SerialTram&>(tram);
         // tram = (SerialTram)tram;
-        st_tram->data[0] = 0;
+        st_tram.data.resize(st_tram.size);  
         _lock.lock();
-        ssize_t ret = read(_fd, (st_tram->data.data() + st_tram->offset), st_tram->size);
+        ssize_t ret = read(_fd, (st_tram.data.data() + st_tram.offset), st_tram.data.size());
         _lock.unlock();
         return ret;
     }
@@ -65,9 +66,9 @@ namespace sonia_common_cpp
 
     ssize_t SerialConn::Transmit(const ITramData &tram)
     {
-        const SerialTram* st_tram = dynamic_cast<const SerialTram*>(&tram);
+        const SerialTram& st_tram = dynamic_cast<const SerialTram&>(tram);
         _lock.lock();
-        ssize_t ret = write(_fd, st_tram->data.data(), st_tram->size);
+        ssize_t ret = write(_fd, st_tram.data.data(), st_tram.size);
         _lock.unlock();
         return ret;
     }
